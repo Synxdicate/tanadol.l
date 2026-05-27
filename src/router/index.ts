70850b13f8d5fcd15e0b3e565ede6657d02dbb6b
Synxdicate <thanadondevsec@gmail.com>
2026-05-27 16:34:04 +0700

    fix error path#1

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../components/home.vue'
+import Home from '../components/Home.vue'
 import About from '../components/about.vue'
-import Project from '../components/project.vue'
-import Article from '../components/article.vue'
+import Project from '../components/Project.vue'
+import Article from '../components/Article.vue'
 import Resume from '../components/resume.vue'
 
 const routes = [
@@ -17,24 +17,28 @@ const routes = [
   },
   {
     path: '/about',
+    name: 'About',
     component: About
   },
   {
     path: '/project',
+    name: 'Projects',
     component: Project
   },
   {
     path: '/article',
+    name: 'Article',
     component: Article
   },
   {
     path: '/resume',
+    name: 'Resume',
     component: Resume
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
